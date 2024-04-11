e99a1c53bf76cae3d390d032de5015a2b194c78a
HamidB <ejecteur@yahoo.fr>
2024-04-11 19:15:04 +0200

    update baseicscodelab

--- basicscodelab/src/main/java/fr/hamtec/basicscodelab/MainActivity.kt
+++ basicscodelab/src/main/java/fr/hamtec/basicscodelab/MainActivity.kt
@@ -6,36 +6,45 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fr.hamtec.basicscodelab.ui.theme.DemoJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState : Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DemoJetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name : String, modifier : Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun MyApp(modifier : Modifier = Modifier){
+    Surface(
+            modifier = modifier,
+            color = MaterialTheme.colorScheme.background
+    ) {
+        Greeting(name = "Android")
+    }
+}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text(
+                text = "Hello $name!",
+                modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
@@ -44,4 +53,9 @@ fun GreetingPreview() {
     DemoJetpackComposeTheme {
         Greeting("Android")
     }
+}
+@Preview(showBackground = true)
+@Composable
+fun MyAppPrerview() {
+    MyApp()
 }
