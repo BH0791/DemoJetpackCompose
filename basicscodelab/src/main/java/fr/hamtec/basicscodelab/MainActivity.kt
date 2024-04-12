fd1ae685081f83542c7bcd295a9231ee9fce2cd8
HamidB <ejecteur@yahoo.fr>
2024-04-12 15:09:02 +0200

    7bis. Gérer les états dans Compose

--- basicscodelab/src/main/java/fr/hamtec/basicscodelab/MainActivity.kt
+++ basicscodelab/src/main/java/fr/hamtec/basicscodelab/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,22 +38,32 @@ fun MyApp(modifier : Modifier = Modifier) {
             modifier = modifier,
             color = MaterialTheme.colorScheme.background
     ) {
-        Greeting(name = "Android")
+        Column {
+            Greeting(name = "Android")
+            Greeting(name = "Compose")
+        }
     }
 }
 
 @Composable
-fun Greeting(name : String, modifier : Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
+fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
 
-
-        Column(modifier = Modifier.padding(24.dp)) {
-            Text(
-                    text = "Hello"
-            )
-            Text(
-                    text = "$name"
-            )
+    Surface(
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f).padding(bottom = extraPadding)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            ElevatedButton(
+                    onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
         }
     }
 }
@@ -58,7 +72,10 @@ fun Greeting(name : String, modifier : Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     DemoJetpackComposeTheme {
-        Greeting("Android")
+        Column {
+            Greeting(name = "Android")
+            Greeting(name = "Compose")
+        }
     }
 }
 
