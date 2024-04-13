14f43d496a1d28d0adfebf155fe0d193cd30df67
HamidB <ejecteur@yahoo.fr>
2024-04-13 14:42:43 +0200

    11. Animer votre liste

--- basicscodelab/src/main/java/fr/hamtec/basicscodelab/MainActivity.kt
+++ basicscodelab/src/main/java/fr/hamtec/basicscodelab/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -73,7 +76,14 @@ private fun Greetings(
 @Composable
 fun Greeting(name : String) {
     var expanded by rememberSaveable { mutableStateOf(false) }
-    val extraPadding = if (expanded) 48.dp else 0.dp
+    //- Animation -> Attention il faut chg le padding(bottom = extraPadding.coerceAtLeast(0.dp))
+    val extraPadding by animateDpAsState(
+            if (expanded) 48.dp else 0.dp,
+            animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+            )
+    )
 
     Surface(
             color = MaterialTheme.colorScheme.primary,
@@ -83,7 +93,7 @@ fun Greeting(name : String) {
             Column(
                     modifier = Modifier
                             .weight(1f)
-                            .padding(bottom = extraPadding)
+                            .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
