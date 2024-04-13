8e3773f32bca7426ef006b4ebda94e0cf76b0d65
HamidB <ejecteur@yahoo.fr>
2024-04-13 19:31:30 +0200

    12. Appliquer un style et un thème à votre application

--- basicscodelab/src/main/java/fr/hamtec/basicscodelab/ui/theme/Theme.kt
+++ basicscodelab/src/main/java/fr/hamtec/basicscodelab/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package fr.hamtec.basicscodelab.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
