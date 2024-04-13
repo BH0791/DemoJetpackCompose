9c0b703129d2017bfd347eb27d5d9b867c5cf553
HamidB <ejecteur@yahoo.fr>
2024-04-13 15:27:21 +0200

    BBBBBBUUUUUUGGGGGG

--- basicscodelab/src/main/java/fr/hamtec/basicscodelab/MainActivity.kt
+++ basicscodelab/src/main/java/fr/hamtec/basicscodelab/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import fr.hamtec.basicscodelab.ui.theme.DemoJetpackComposeTheme
@@ -44,7 +45,7 @@ class MainActivity : ComponentActivity() {
 }
 
 /* ++++++++++++++++++++++++++++++++++++++++++
-   ++          TP-Jetpack_Compose          ++
+   ++          TP-Jetpack_Compose          ++       +++
    ++++++++++++++++++++++++++++++++++++++++++
  */
 @Composable
@@ -83,7 +84,7 @@ fun Greeting(name : String) {
                     dampingRatio = Spring.DampingRatioMediumBouncy,
                     stiffness = Spring.StiffnessLow
             )
-    )
+    ) 
 
     Surface(
             color = MaterialTheme.colorScheme.primary,
@@ -96,7 +97,11 @@ fun Greeting(name : String) {
                             .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = name,
+                        style = MaterialTheme.typography.headlineMedium.copy(
+                                fontWeight = FontWeight.ExtraBold
+                        )
+                        )
             }
             ElevatedButton(
                     onClick = { expanded = ! expanded }
