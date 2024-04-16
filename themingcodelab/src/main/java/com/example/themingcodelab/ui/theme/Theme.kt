45f1c9918bce2d68434e1f9558d7952a7ec4e92b
HamidB <ejecteur@yahoo.fr>
2024-04-16 06:23:29 +0200

    5. Ajouter des couleurs dynamiques dans l'application

--- themingcodelab/src/main/java/com/example/themingcodelab/ui/theme/Theme.kt
+++ themingcodelab/src/main/java/com/example/themingcodelab/ui/theme/Theme.kt
@@ -9,7 +9,11 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = md_theme_dark_primary,
@@ -101,7 +105,17 @@ fun DemoJetpackComposeTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-
+// Ajouter la couleur principale de la barre d'état à partir de la palette de couleurs choisie.
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat
+                .getInsetsController(window, view)
+                .isAppearanceLightStatusBars = darkTheme
+        }
+    }
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
