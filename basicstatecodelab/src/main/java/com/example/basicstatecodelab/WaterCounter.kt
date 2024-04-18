d2f227cd244215781b74cdc8579b2e448f60cab8
HamidB <ejecteur@yahoo.fr>
2024-04-18 07:13:09 +0200

    La mémoire dans une fonction composable

--- basicstatecodelab/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ basicstatecodelab/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -5,6 +5,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
@@ -13,15 +18,16 @@ import com.example.basicstatecodelab.ui.theme.DemoJetpackComposeTheme
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
-        Text(text = "You've had $count glasses.")
-        Button(
-            onClick = { count++ },
-            Modifier.padding(top = 8.dp)
-        ) {
+
+        var count by remember { mutableStateOf(0) }
+
+        Text(text = "You've had ${count} glasses.")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text(text = "Add one")
         }
+
     }
+
 }
 
 @Preview(showBackground = true)
