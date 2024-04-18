412880471fb65c7fb3c76309ea121d1149a9f8c8
HamidB <ejecteur@yahoo.fr>
2024-04-18 07:31:30 +0200

    L'UI basée sur l'état

--- basicstatecodelab/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ basicstatecodelab/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -14,6 +15,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
 import com.example.basicstatecodelab.ui.theme.DemoJetpackComposeTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
@@ -22,7 +25,10 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         var count by remember { mutableStateOf(0) }
 
         Text(text = "You've had ${count} glasses.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        Button(
+            onClick = { count++  },
+            Modifier.padding(top = 8.dp)
+        ) {
             Text(text = "Add one")
         }
 
