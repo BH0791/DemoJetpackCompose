c4c785bafef0950d4280a23214857eecdf0ccb05
HamidB <ejecteur@yahoo.fr>
2024-04-25 13:43:55 +0200

    Présentation de l'application de démarrage

--- training-tip-calculator/src/main/java/com/example/training_tip_calculator/MainActivity.kt
+++ training-tip-calculator/src/main/java/com/example/training_tip_calculator/MainActivity.kt
@@ -4,13 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.training_tip_calculator.ui.theme.DemoJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +12,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DemoJetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "XMAN",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DemoJetpackComposeTheme {
-        Greeting("Android")
     }
 }
