b7ed6c5d2f1ac8643a3e338dfd96cef8f52c22cb
HamidB <ejecteur@yahoo.fr>
2024-04-25 16:15:12 +0200

    Modifier l'apparence

--- training-tip-calculator/src/main/java/com/example/training_tip_calculator/MainActivity.kt
+++ training-tip-calculator/src/main/java/com/example/training_tip_calculator/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -25,11 +26,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.training_tip_calculator.ui.theme.DemoJetpackComposeTheme
 import java.text.NumberFormat
-// Changement d'état et recomposition à l'œuvre
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,7 +83,10 @@ fun EditNumberField(
     TextField(
         value = amountInput,
         onValueChange = { amountInput = it },
-        modifier = modifier
+        label = {Text(stringResource(R.string.bill_amount))},
+        singleLine = true,
+        modifier = modifier,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
     )
 }
 
